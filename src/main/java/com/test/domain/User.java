5cdebaf76cb880c8b566e77ef657811982fba696
Oksana Bosenko <skybearf@gmail.com>
2019-07-31 20:44:50 +0300

    @table annotation added to get rid of the Error executing DDL via JDBC Statement

--- src/main/java/com/test/domain/User.java
+++ src/main/java/com/test/domain/User.java
@@ -1,15 +1,12 @@
 package com.test.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-
+@Table(name = "users")
 @Entity // This tells Hibernate to make a table out of this class
 public class User {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     private String name;
